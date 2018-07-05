f34d21bb4d3f15397968d7ba1fc3fefc8fbc600a
hcarneiro <hcarneiro@weboo.biz>
2018-07-05 22:26:13 +0100

    Updates

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,13 @@
           target="_blank"
           class="button--grey">GitHub</a>
       </div>
+      <select v-model="selected">
+        <option disabled value="">Please select one</option>
+        <option>A</option>
+        <option>B</option>
+        <option>C</option>
+      </select>
+      <span>Selected: {{ selected }}</span>
     </div>
   </section>
 </template>
@@ -26,39 +33,13 @@
 import AppLogo from '~/components/AppLogo.vue'
 
 export default {
+  data() {
+    return {
+      selected: ''
+    }
+  },
   components: {
     AppLogo
   }
 }
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
