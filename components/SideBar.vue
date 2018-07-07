e8fae177a56228d6323eab7b279652d269142c02
hcarneiro <hcarneiro@weboo.biz>
2018-07-07 01:52:32 +0100

    Initial page structure

---
+++ components/SideBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="sidebar-wrapper">
+    <div class="sidebar-logo">
+      <h1><span>2</span>DO</h1>
+    </div>
+    <hr>
+    <ul class="sidebar-list">
+      <li class="sidebar-list-item">
+        <i class="far fa-star fa-fw"></i> Today
+      </li>
+      <li class="sidebar-list-item">
+        <i class="far fa-calendar-alt fa-fw"></i> Upcoming
+      </li>
+      <li class="sidebar-list-item">
+        <i class="fas fa-check fa-fw"></i> Completed
+      </li>
+      <li class="sidebar-list-item">
+        <i class="far fa-trash-alt fa-fw"></i> Trash
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,23 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        hugodesigns
-      </h1>
-      <h2 class="subtitle">
-        My first Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-      <select v-model="selected">
-        <option disabled value="">Please select one</option>
-        <option>A</option>
-        <option>B</option>
-        <option>C</option>
-      </select>
-      <span>Selected: {{ selected }}</span>
+  <div class="page-container">
+    <div class="sidebar-component">
+      <side-bar/>
     </div>
-  </section>
+    <div class="view-component">
+    </div>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import SideBar from '~/components/SideBar.vue'
 
 export default {
   data() {
     return {
-      selected: ''
     }
   },
   components: {
-    AppLogo
+    SideBar
   }
 }
 </script>
