3f8b466e7363da8301add38ebbec80cd294ed739
hcarneiro <hcarneiro@weboo.biz>
2018-07-13 00:54:25 +0100

    Created new pages

---
+++ pages/index/today.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="tasks-view-holder">
+    <div class="tasks-header">
+      <h3 class="tasks-title">Today</h3>
+      <div class="tasks-controls">
+        <div class="btn btn-primary custom-btn">Add task</div>
+      </div>
+    </div>
+    <div class="tasks-container">
+      <ul>
+        <template v-for="users in users">
+          <nuxt-link class="task-item" tag="li" :key="users.id" :to="'/users/'+users.id">
+            <i class="fas fa-grip-vertical fa-fw rearrange"></i>
+            <i class="far fa-check-circle markable"></i>
+            <p>{{ users.name }}</p>
+          </nuxt-link>
+        </template>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+
+export default {
+  async asyncData () {
+    let { data } = await axios.get('/api/users')
+    return { users: data }
+  },
+  head () {
+    return {
+      title: '2DO - Today'
+    }
+  }
+}
+</script>
---
+++ pages/index/upcoming.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="tasks-view-holder">
+    <div class="tasks-header">
+      <h3 class="tasks-title">Upcoming</h3>
+      <div class="tasks-controls">
+        <div class="btn btn-primary custom-btn">Add task</div>
+      </div>
+    </div>
+    <div class="tasks-container">
+      <ul>
+        <li class="task-item">
+          <i class="fas fa-grip-vertical fa-fw rearrange"></i>
+          <i class="far fa-check-circle markable"></i>
+          <p>My new task for today</p>
+        </li>
+        <li class="task-item">
+          <i class="fas fa-grip-vertical fa-fw rearrange"></i>
+          <i class="far fa-check-circle markable"></i>
+          <p>My new task for today</p>
+        </li>
+        <li class="task-item">
+          <i class="fas fa-grip-vertical fa-fw rearrange"></i>
+          <i class="fas fa-check-circle markable"></i>
+          <p>My new task for today</p>
+        </li>
+        <li class="task-item">
+          <i class="fas fa-grip-vertical fa-fw rearrange"></i>
+          <i class="far fa-check-circle markable"></i>
+          <p>My new task for today</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  head () {
+    return {
+      title: '2DO - Upcoming'
+    }
+  }
+}
+</script>
