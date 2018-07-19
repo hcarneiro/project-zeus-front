ffa58c9899b616e85aba36ff7201e42091ddc7fa
hcarneiro <hcarneiro@weboo.biz>
2018-07-20 00:48:13 +0100

    created first resouce to get list of users

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,36 +1,7 @@
-const { Router } = require('express')
+import axios from '~/plugins/axios'
 
-const router = Router()
-
-// Mock Users
-const users = [
-  {
-    name: 'Alexandre',
-    id: 1
-  },
-  {
-    name: 'Pooya',
-    id: 2
-  },
-  {
-    name: 'Sébastien',
-    id: 3
+export default {
+  getUsers() {
+    return axios.get('/users')
   }
-]
-
-/* GET users listing. */
-router.get('/users', function (req, res, next) {
-  res.json(users)
-})
-
-/* GET user by ID. */
-router.get('/users/:id', function (req, res, next) {
-  const id = parseInt(req.params.id)
-  if (id >= 0 && id < users.length) {
-    res.json(users[id])
-  } else {
-    res.sendStatus(404)
-  }
-})
-
-module.exports = router
+}
