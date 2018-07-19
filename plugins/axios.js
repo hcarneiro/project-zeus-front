1276965af9613cde9efd03f9d7ba1d5b19308458
hcarneiro <hcarneiro@weboo.biz>
2018-07-20 00:54:07 +0100

    unnecessary if statement

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,7 @@
-import * as axios from 'axios'
+import axios from 'axios'
 
 let options = {}
 // The server-side needs a full url to works
-if (process.server) {
-  options.baseURL = `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3333}`
-}
+options.baseURL = `http://${process.env.API_URL || 'localhost'}:${process.env.API_PORT || 5000}`
 
 export default axios.create(options)
