40b89d2ab11ac0cc99bdddf34c5a803e6a26bf0d
hcarneiro <hcarneiro@weboo.biz>
2018-08-02 01:03:27 +0100

    added route to create tasks

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -1,6 +1,11 @@
 import axios from '~/plugins/axios'
 
 export default {
+  addTask(data) {
+    return axios.post('/v1/tasks', data).then((response) => {
+      return Promise.resolve(response.data)
+    })
+  },
   getTasks() {
     return axios.get('/v1/tasks').then((response) => {
       return Promise.resolve(response.data)
