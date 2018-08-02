741a90a2b4f7850496e37515b0e21a58d7b30d15
hcarneiro <hcarneiro@weboo.biz>
2018-08-02 01:03:59 +0100

    Testing websockets for new tasks

--- pages/index/my-tasks.vue
+++ pages/index/my-tasks.vue
@@ -3,6 +3,7 @@
     <top-menu :title="'My Tasks'"/>
     <div class="tasks-view-holder">
       <div class="tasks-container">
+        <button class="btn btn-primary" v-on:click.prevent="sendTask">New task</button>
         <ul>
           <div v-for="project in projects" :key="project.id" class="project-holder">
             <h4 class="task-project-title">{{ project.name }}</h4>
@@ -10,7 +11,7 @@
               <div class="task-controls">
                 <div class="task-status-date" v-bind:class="task.status | convertToClass">
                   <div class="task-status">{{ task.status }}</div>
-                  <div class="task-date">{{ task.dueDate | dueDate }}</div>
+                  <div class="task-date">{{ task.dueAt | formatDate }}</div>
                 </div>
                 <div class="task-options">
                   <div class="btn complete-toggle">Mark as completed</div>
@@ -31,6 +32,11 @@
 <script>
 import TopMenu from '~/components/TopMenu.vue'
 import projects from '~/api/routes/projects'
+import tasks from '~/api/routes/tasks'
+import { orderBy } from 'lodash'
+import io from 'socket.io-client'
+
+const socket = io(`http://${process.env.API_URL || 'localhost'}:${process.env.API_PORT || 5000}`)
 
 export default {
   async asyncData () {
@@ -46,6 +52,30 @@ export default {
   },
   components: {
     TopMenu
+  },
+  methods: {
+    sendTask() {
+      const data = {
+        "title": "Hello",
+        "status": "In progress",
+        "projectId": 2
+      }
+
+      tasks.addTask(data)
+        .then((task) => {
+          socket.emit('NEW TASK', task);
+        })
+    }
+  },
+  mounted() {
+    socket.on('TASK', (data) => {
+      this.projects.forEach((project, i) => {
+        if (project.id === data.projectId) {
+          this.projects[i].tasks.push(data)
+          orderBy(this.projects[i].tasks, ['createdAt'], ['desc'])
+        }
+      })
+    });
   }
 }
 </script>
