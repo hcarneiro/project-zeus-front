3888a09d8034fab98152356132292dde32fe2db6
hcarneiro <hcarneiro@weboo.biz>
2018-08-05 19:07:09 +0100

    tweak

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,11 +5,11 @@ const routesToSkipAuth = [
 ];
 
 export default function ({ store, route, redirect }) {
-  if (!store.state.auth.authenticated && routesToSkipAuth.indexOf(route.fullPath) < 0) {
-    return redirect('301', '/login')
-  }
-
   if (route.fullPath == '/') {
     return redirect('301', '/my-tasks')
+  }
+
+  if (!store.state.auth.authenticated && routesToSkipAuth.indexOf(route.fullPath) < 0) {
+    return redirect('301', '/login')
   }
 }
