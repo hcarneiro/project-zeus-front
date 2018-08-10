16e3ba5c07309576ec47439c88d0db902e6e4244
hcarneiro <hcarneiro@weboo.biz>
2018-08-11 00:22:28 +0100

    Removed unnecessary plugin

--- plugins/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-let options = {}
-options.baseURL = `http://${process.env.API_URL || 'localhost'}:${process.env.API_PORT || 5000}`
-
-export default axios.create(options)
