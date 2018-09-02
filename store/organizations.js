d5a93ef5484e433165856b730150ee60828bc5b0
hcarneiro <hcarneiro@weboo.biz>
2018-09-02 16:55:10 +0100

    Set organization users

--- store/organizations.js
+++ store/organizations.js
@@ -1,3 +1,5 @@
+import { forEach } from 'lodash'
+
 export const state = () => ({
   userOrganizations: []
 })
@@ -5,14 +7,32 @@ export const state = () => ({
 export const mutations = {
   setUserOrganizations(state, organizations) {
     state.userOrganizations = organizations
+  },
+  setOrganizationUsers(state, data) {
+    forEach(state.userOrganizations, (organization) => {
+      if (organization.id === data.organizationId) {
+        organization.currentUsers = data.users
+      }
+    })
   }
 }
 
 export const actions = {
   get({commit}) {
     return this.$axios.get('v1/organizations').then((response) => {
+      const promises = []
       commit('setUserOrganizations', response.data.organizations)
-      return Promise.resolve()
+      response.data.organizations.forEach((organization) => {
+        promises.push(
+          this.$axios.get(`v1/organizations/${organization.id}/users`).then((response) => {
+            commit('setOrganizationUsers', {
+              users: response.data.users,
+              organizationId: organization.id
+            })
+          })
+        )
+      })
+      return Promise.all(promises)
     })
   }
 }
