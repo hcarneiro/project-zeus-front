9d32a7f5b4cf61e6f28b0400adc2a3921347c88a
hcarneiro <hcarneiro@weboo.biz>
2019-03-01 23:43:40 +0000

    Temp page

--- pages/login.vue
+++ pages/login.vue
@@ -6,42 +6,7 @@
     </div>
     <div class="auth-holder">
       <h1>Welcome!</h1>
-      <p class="text-muted">Login to your account</p>
-      <form @submit.prevent="login">
-        <div class="auth-form">
-          <div class="form-group auth-form-field">
-            <label for="login-email">Email address</label>
-            <input type="email" class="form-control" id="login-email" v-model="email" required>
-            <i class="fas fa-user"></i>
-          </div>
-          <div class="form-group auth-form-field">
-            <label for="login-password">Password</label>
-            <input type="password" class="form-control" id="login-password" v-model='password' required>
-            <i class="fas fa-lock"></i>
-          </div>
-        </div>
-        <div class="auth-aux-controls">
-          <div class="checkbox-icon">
-            <label class="checkbox-label" for="remember-me">Remember me
-              <input type="checkbox" id="remember-me" v-model="remember">
-              <div class="checkbox-mark">
-                <i class="fas fa-check"></i>
-              </div>
-            </label>
-          </div>
-          <div>
-            <nuxt-link tag="a" :to="'/forgot-password'">Forgot password?</nuxt-link>
-          </div>
-        </div>
-        <div class="bd-callout bd-callout-danger" v-if="error" v-html="error"></div>
-        <div class="auth-btn-controls">
-          <button type="submit" class="btn btn-primary"  v-bind:class="{ 'disabled': isAuthenticating }">
-            <span v-if="isAuthenticating">Please wait...</span>
-            <span v-else>Login</span>
-          </button>
-          <nuxt-link class="btn btn-secondary" tag="button" :to="'/signup'">Sign up</nuxt-link>
-        </div>
-      </form>
+      <p class="text-muted">This platform is still being developed!</p>
     </div>
   </div>
 </template>
